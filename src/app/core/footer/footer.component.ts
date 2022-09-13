593e8a724e6786bbce98d38d7e8fb05a23f1f79d
dev-apiarist <klesliedev@gmail.com>
2022-09-13 08:28:49 -0500

    minor changes

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,15 +1,16 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  constructor(private viewPortScroller: ViewportScroller) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  public onClick(elementId: string): void {
+    this.viewPortScroller.scrollToAnchor(elementId);
   }
-
 }
