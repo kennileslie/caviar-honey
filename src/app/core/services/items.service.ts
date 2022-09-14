f9945818a24a8c6eea077fe259d7a0da3152afaf
dev-apiarist <klesliedev@gmail.com>
2022-09-14 10:15:11 -0500

    url update (again)

--- src/app/core/services/items.service.ts
+++ src/app/core/services/items.service.ts
@@ -9,7 +9,7 @@ import { catchError, Observable, of } from 'rxjs';
   providedIn: 'root',
 })
 export class ItemsService {
-  private API_URL: string = 'https://caviar-honey.herokuapp.com/items/';
+  private API_URL: string = 'https://caviar-honey.herokuapp.com/api/v1/items/';
 
   constructor(private http: HttpClient, private authService: AuthService) {}
 
